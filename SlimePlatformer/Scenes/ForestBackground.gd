82273af70040314059fbe4321f55def2480ab632
Gabrielius Gintalas <gabrieliusgintalas@csu.fullerton.edu>
2024-02-21 15:47:37 -0800

    Working on creating forest background

---
+++ SlimePlatformer/Scenes/ForestBackground.gd
@@ -0,0 +1,14 @@
+extends Node2D
+export(NodePath) var player_path: NodePath
+var player: CharacterBody2D
+
+var speed: float = 100.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
